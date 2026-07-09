917192022213af9d9ebf8276684c8da03be79748
MikeAngelo1991 <sirmiguel28@gmail.com>
2026-07-08 23:11:55 -0500

    MoreValidations.spec.js final

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -14,10 +14,16 @@ test('Popup validations', async ({ page }) => {
     page.on("dialog", dialog => dialog.accept()); // se acepta el cuadro de dialogo
     await page.locator("#confirmbtn").click(); // se hace clic en el boton de confirmacion 
     
-    page.on("dialog", dialog => dialog.dismiss()); // se descarta el cuadro de dialogo
-    await page.locator("#confirmbtn").click(); // se hace clic en el boton de confirmacion 
+    //page.on("dialog", dialog => dialog.dismiss()); // se descarta el cuadro de dialogo
+    //await page.locator("#confirmbtn").click(); // se hace clic en el boton de confirmacion 
 
     await page.locator("#mousehover").hover(); // se hace hover sobre el elemento
     await page.locator(".mouse-hover-content a").last().click(); // se hace clic en el ultimo elemento del menu desplegable
+
+    const framespage = page.frameLocator("#courses-iframe"); // se busca el iframe por su id
+    framespage.locator("li a[href*='lifetime-access']:visible").click(); // se hace clic en el enlace de acceso de por vida dentro del iframe visible
     
+    const textCheck = await framespage.locator(".text h2").textContent(); // se obtiene el texto del elemento dentro del iframe
+    console.log(textCheck.split(" ")[1]); // se divide el texto en un array y se obtiene el segundo elemento del array
+
 })
