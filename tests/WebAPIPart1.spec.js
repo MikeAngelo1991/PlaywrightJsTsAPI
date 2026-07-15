2af829be2ab5f9db88efd95847e1b792f83e42c0
MikeAngelo1991 <sirmiguel28@gmail.com>
2026-07-15 15:08:39 -0500

    ApiUtils.js y WebAPIPart1 ok

--- tests/WebAPIPart1.spec.js
+++ tests/WebAPIPart1.spec.js
@@ -1,72 +1,46 @@
 const { test, expect, request } = require('@playwright/test');
+const { ApiUtils } = require('./utils/ApiUtils.js'); // se importa la clase ApiUtils desde el archivo ApiUtils.js
 
 const loginPayLoad = { userEmail: "sirmiguel28@gmail.com", userPassword: "Inmamm34@@" }
 let token; // variable global para almacenar el token de autenticación
 const orderPayload = { "orders": [{ "country": "Cuba", "productOrderedId": "6960eac0c941646b7a8b3e68" }] }
-let orderId; // variable global para almacenar el id de la orden generada
+//let orderId; // variable global para almacenar el id de la orden generada
 
+let response; // variable global para almacenar la respuesta de la solicitud de creación de orden
 test.beforeAll(async () => // se ejecuta antes de todos los tests
 {
     // login API call to get the token
     const apiContext = await request.newContext(); // se crea un nuevo contexto de solicitud para realizar solicitudes HTTP
-
-
-    // generar la compra y validar el id de la orden generada en la tabla de ordenes
-
-    const orderResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order",
-        {
-            data: orderPayload,
-            headers: {
-                'Authorization': token,
-                'Content-Type': 'application/json'
-            },
-
-        });
-        
-        const orderResponseJson = await orderResponse.json(); // se obtiene el cuerpo de la respuesta de la solicitud en formato JSON
-        orderResponse.ok(); // se verifica que la respuesta de la solicitud sea exitosa
-        console.log(orderResponseJson); // se imprime la respuesta de la solicitud en la consola
-        orderId = orderResponseJson.orders[0]; // se obtiene el id de la orden generada
-
-});
-
-test.beforeEach(() => // se ejecuta antes de cada test
-{
-
-
+    const apiUtils = new ApiUtils(apiContext, loginPayLoad); // se crea una instancia de la clase ApiUtils y se pasa el contexto de la solicitud y el cuerpo de la solicitud como parámetros
+    response = await apiUtils.creatOrder(orderPayload); // se llama a la función creatOrder de la clase ApiUtils y se pasa el cuerpo de la solicitud como parámetro, se espera a que se resuelva la promesa y se almacena el id de la orden generada en la variable response
 
 });
 
 // test 1, test 2, test 3
-
 
 //create order is successful and verify it in the orders page
 
     test('Place the order', async ({ page }) => { // se colcoca browser para abrir el navegador
 
 
-        const ApiUtils = new ApiUtils() 
-        const orderId = creatOrder(); // 
+        //const apiUtils = new ApiUtils(response.apiContext, loginPayLoad); // se crea una instancia de la clase ApiUtils y se pasa el contexto de la solicitud y el cuerpo de la solicitud como parámetros
+        //const response = await apiUtils.creatOrder(orderPayload); // se llama a la función creatOrder de la clase ApiUtils y se pasa el cuerpo de la solicitud como parámetro, se espera a que se resuelva la promesa y se almacena el id de la orden generada en la variable response
+        //const orderId = response.orderId;
 
-        page.addInitScript(value => { //|
+        page.addInitScript(value => { // se agrega un script de inicialización a la página para establecer el token de autenticación en el almacenamiento local del navegador
             window.localStorage.setItem('token', value); // se establece el token de autenticación en el almacenamiento local del navegador
-        }, token);
+        }, response.token);
        
         const email = "sirmiguel28@gmail.com";
-        await page.goto("https://rahulshettyacademy.com/client");
-        const productName = "ZARA COAT 3";
-        const products = page.locator(".card-body"); // se busca el elemento del contenedor de productos
-
-       
+        await page.goto("https://rahulshettyacademy.com/client"); 
         await page.locator("button[routerlink*=myorders]").click(); // se hace clic en el enlace de mis ordenes
-
         await page.locator("tbody").waitFor(); // espera a que el elemento este visible en la pagina
         const rows = await page.locator("tbody tr");
 
 
         for (let i = 0; i < await rows.count(); ++i) { // se recorre el numero de filas de la tabla
             const rowOrderId = await rows.nth(i).locator("th").textContent(); // se obtiene el texto del elemento que contiene el id de la orden
-            if (orderId.includes(rowOrderId)) { // se compara el id de la orden generada con el id de la orden en la tabla
+            if (response.orderId.includes(rowOrderId)) { // se compara el id de la orden generada con el id de la orden en la tabla
                 await rows.nth(i).locator("button").first().click(); // se hace clic en el boton de la fila que contiene el id de la orden
                 break; // se rompe el ciclo una vez que se encuentra el id de la orden y se hace clic en el boton
             }
@@ -74,7 +48,7 @@ test.beforeEach(() => // se ejecuta antes de cada test
 
         const orderIdDetails = await page.locator(".col-text").textContent(); // se obtiene el texto del elemento que contiene el id de la orden en la pagina de detalles de la orden
         await page.pause(); // se pausa la ejecución del test para poder inspeccionar la página
-        await expect(orderId.includes(orderIdDetails)).toBeTruthy(); // se verifica que el id de la orden generada sea el mismo que el id de la orden en la pagina de detalles de la orden
+        await expect(response.orderId.includes(orderIdDetails)).toBeTruthy(); // se verifica que el id de la orden generada sea el mismo que el id de la orden en la pagina de detalles de la orden
 
     });
 
--- tests/utils/ApiUtils.js
+++ tests/utils/ApiUtils.js
@@ -1,23 +1,46 @@
 class ApiUtils {
 
 
-    constructor(apiContext)
+    constructor(apiContext, loginPayLoad) // se crea un constructor para inicializar la clase ApiUtils y se pasa el contexto de la solicitud como parámetro
     {
-
+        this.apiContext = apiContext; // se asigna el contexto de la solicitud a una propiedad de la clase ApiUtils
+        this.loginPayLoad = loginPayLoad; // se asigna el cuerpo de la solicitud de login a una propiedad de la clase ApiUtils
     }
 
-    async getToken() {
-        const loginResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login",
+    async getToken() {// se crea una función asincrónica para obtener el token de autenticación
+
+        const loginResponse = await this.apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login",
             {
-                data: loginPayLoad // se envía el cuerpo de la solicitud en formato JSON
+                data: this.loginPayLoad // se envía el cuerpo de la solicitud en formato JSON
             }
         );
         // 200, 201
 
-        expect(loginResponse.ok()).toBeTruthy(); // se verifica que la respuesta de la solicitud sea exitosa
+        //expect(loginResponse.ok()).toBeTruthy(); // se verifica que la respuesta de la solicitud sea exitosa
         const loginResponseJson = await loginResponse.json(); // se obtiene el cuerpo de la respuesta de la solicitud en formato JSON
-        token = loginResponseJson.token; // se obtiene el token de la respuesta de la solicitud
+        const token = loginResponseJson.token; // se obtiene el token de la respuesta de la solicitud
         console.log(token); // se imprime el token en la consola
         return token;
     }
+
+    async creatOrder(orderPayload) { // se crea una función asincrónica para generar una orden
+
+        let response = {};
+        response.token = await this.getToken(); // se llama a la función getToken() para obtener el token de autenticación y se almacena en la variable response
+        const orderResponse = await this.apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order",
+            {
+                data: orderPayload,
+                headers: {
+                    'Authorization': response.token, // se establece el token de autenticación en el encabezado de la solicitud
+                    'Content-Type': 'application/json'
+                },
+            });
+        const orderResponseJson = await orderResponse.json(); // se obtiene el cuerpo de la respuesta de la solicitud en formato JSON
+        console.log(orderResponseJson); // se imprime la respuesta de la solicitud en la consola
+        const orderId = orderResponseJson.orders[0]; // se obtiene el id de la orden generada 
+        response.orderId = orderId; // se almacena el id de la orden generada en la variable response
+        return response;
+    }
 }
+
+module.exports = { ApiUtils };
