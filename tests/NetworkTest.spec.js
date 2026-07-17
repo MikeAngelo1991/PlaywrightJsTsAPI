19ff47ba1065ddfaf08db905911391e7743b02cc
Miguel Angel Muñoz Montoya <miguelmunoz@seti.com.co>
2026-07-17 08:04:06 -0500

    NetworkTest.spec.js interceptar response done

--- tests/NetworkTest.spec.js
+++ tests/NetworkTest.spec.js
@@ -4,7 +4,8 @@ const { ApiUtils } = require('./utils/ApiUtils.js'); // se importa la clase ApiU
 const loginPayLoad = { userEmail: "sirmiguel28@gmail.com", userPassword: "Inmamm34@@" }
 let token; // variable global para almacenar el token de autenticación
 const orderPayload = { "orders": [{ "country": "Cuba", "productOrderedId": "6960eac0c941646b7a8b3e68" }] }
-//let orderId; // variable global para almacenar el id de la orden generada
+const fakePayLoadOrders = { data: [], message: "No Orders" } // variable global para almacenar la respuesta de la solicitud de creación de orden
+
 
 let response; // variable global para almacenar la respuesta de la solicitud de creación de orden
 test.beforeAll(async () => // se ejecuta antes de todos los tests
@@ -20,35 +21,36 @@ test.beforeAll(async () => // se ejecuta antes de todos los tests
 
 //create order is successful and verify it in the orders page
 
-    test('Place the order', async ({ page }) => { // se colcoca browser para abrir el navegador
+test('@SP Place the order', async ({ page }) => { // se colcoca browser para abrir el navegador
 
+    page.addInitScript(value => { // se agrega un script de inicialización a la página para establecer el token de autenticación en el almacenamiento local del navegador
+        window.localStorage.setItem('token', value); // se establece el token de autenticación en el almacenamiento local del navegador
+    }, response.token);
 
-        //const apiUtils = new ApiUtils(response.apiContext, loginPayLoad); // se crea una instancia de la clase ApiUtils y se pasa el contexto de la solicitud y el cuerpo de la solicitud como parámetros
-        //const response = await apiUtils.creatOrder(orderPayload); // se llama a la función creatOrder de la clase ApiUtils y se pasa el cuerpo de la solicitud como parámetro, se espera a que se resuelva la promesa y se almacena el id de la orden generada en la variable response
-        //const orderId = response.orderId;
+    const email = "sirmiguel28@gmail.com";
+    await page.goto("https://rahulshettyacademy.com/client");
 
-        page.addInitScript(value => { // se agrega un script de inicialización a la página para establecer el token de autenticación en el almacenamiento local del navegador
-            window.localStorage.setItem('token', value); // se establece el token de autenticación en el almacenamiento local del navegador
-        }, response.token);
-       
-        const email = "sirmiguel28@gmail.com";
-        await page.goto("https://rahulshettyacademy.com/client"); 
+    // se esta interceptando la respuesta de la solicitud de obtener el conteo del carrito de compras y se esta reemplazando con una respuesta falsa que indica que no hay ordenes
+    await page.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/*",
+        async route => {
 
-        page.route("https://rahulshettyacademy.com/api/ecom/user/get-cart-count/68fee61ff669d6cb0a2e44c7", 
-            route => {
+            const response = await page.request.fetch(route.request());
+            let body = JSON.stringify(fakePayLoadOrders); // se convierte el objeto fakePayLoadOrders a una cadena JSON
+            route.fulfill({
+                response,
+                body,
 
-                const response = page.request.fetch(route.request());
-                route.fulfill({
-                    
-                })
-                //interceptin response - API response -> { Playwright fakereponse } -> browser->render -> render data on front 
-            }
-        );
-        await page.locator("button[routerlink*=myorders]").click(); // se hace clic en el enlace de mis ordenes
-        await page.locator("tbody").waitFor(); // espera a que el elemento este visible en la pagina
-        const rows = await page.locator("tbody tr");
+            })
+            //intercepting response - API response -> { Playwright fake response } -> browser->render -> render data on front
+        }
+    );
+    await page.pause(); // se pausa la ejecución del test para poder inspeccionar la página
+    await page.locator("button[routerlink*=myorders]").click(); // se hace clic en el enlace de mis ordenes
+    await page.waitForResponse("https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/*"); // espera a que se reciba la respuesta de la solicitud de obtener las ordenes del cliente
 
-    });
+    console.log(await page.locator(".mt-4").textContent());
+
+});
 
 //Verify if the order is showing in the history page
 //Precondition - create an order
